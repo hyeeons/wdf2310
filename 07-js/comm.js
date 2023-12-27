867e7a4130daa15f84c85d4746723262ca3d3fca
hyeeons <qouop0101@gmail.com>
2023-12-27 12:20:57 +0900

    js

---
+++ 07-js/comm.js
@@ -0,0 +1,23 @@
+// 변수
+// 데이터를 저장하고 사용하는 데이터의 이름
+// var, let, const
+// var은 현재 잘 사용하지 않음
+
+// 변수 선언
+let a = 10;
+const b = 5;
+
+console.log(a);
+console.log(b);
+console.log(a + b);
+console.log(a - b);
+console.log(a / b);
+console.log(a * b);
+
+// let 변수 (변수의 값을 변경할 수 있음)
+let c = 10;
+console.log(c);
+
+// 변수값 변경
+c = 20;
+console.log(c);
